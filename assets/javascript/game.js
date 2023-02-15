564bfc28e1af658370d3f877e379277e6848522b
Alex Lilly <bigbuckalex@gmail.com>
2023-02-15 01:52:01 -0500

    change formatting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,126 +1,129 @@
-const possibleWords =  ["area",
-    "book",
-    "business",
-    "case",
-    "child",
-    "company",
-    "country",
-    "day",
-    "eye",
-    "fact",
-    "family",
-    "government",
-    "group",
-    "hand",
-    "home",
-    "job",
-    "life",
-    "lot",
-    "man",
-    "money",
-    "month",
-    "mother",
-    "night",
-    "number",
-    "part",
-    "people",
-    "place",
-    "point",
-    "problem",
-    "program",
-    "question",
-    "right",
-    "room",
-    "school",
-    "state",
-    "story",
-    "student",
-    "study",
-    "system",
-    "thing",
-    "time",
-    "water",
-    "way",
-    "week",
-    "woman",
-    "word",
-    "work",
-    "world",
-    "year"];
-let wins = 0;
-let lettersGuessed = [];
-let guessesRemaining = 10;
-let winningWord = "";
-let blankWord = "";
-let word = "";
+const possibleWords = [
+  "area",
+  "book",
+  "business",
+  "case",
+  "child",
+  "company",
+  "country",
+  "day",
+  "eye",
+  "fact",
+  "family",
+  "government",
+  "group",
+  "hand",
+  "home",
+  "job",
+  "life",
+  "lot",
+  "man",
+  "money",
+  "month",
+  "mother",
+  "night",
+  "number",
+  "part",
+  "people",
+  "place",
+  "point",
+  "problem",
+  "program",
+  "question",
+  "right",
+  "room",
+  "school",
+  "state",
+  "story",
+  "student",
+  "study",
+  "system",
+  "thing",
+  "time",
+  "water",
+  "way",
+  "week",
+  "woman",
+  "word",
+  "work",
+  "world",
+  "year",
+]
+let wins = 0
+let lettersGuessed = []
+let guessesRemaining = 10
+let winningWord = ""
+let blankWord = ""
+let word = ""
 
-function win(){
-    wins++;
-    document.getElementById("wins").innerHTML = wins;
-    lettersGuessed = [];
-    guessesRemaining = 10;
-    lettersGuessed = [];
-    winningWord = "";
-    blankWord = "";
-    word = "";
-    selectWord();
-    document.getElementById("blanks").innerHTML = blankWord;
-    document.getElementById("numGuesses").innerHTML = guessesRemaining;
-    document.getElementById("letGuesses").innerHTML = "~";
+function win() {
+  wins++
+  document.getElementById("wins").innerHTML = wins
+  lettersGuessed = []
+  guessesRemaining = 10
+  lettersGuessed = []
+  winningWord = ""
+  blankWord = ""
+  word = ""
+  selectWord()
+  document.getElementById("blanks").innerHTML = blankWord
+  document.getElementById("numGuesses").innerHTML = guessesRemaining
+  document.getElementById("letGuesses").innerHTML = "~"
 }
 
-function lose(){
-    lettersGuessed = [];
-    guessesRemaining = 10;
-    lettersGuessed = [];
-    winningWord = "";
-    blankWord = "";
-    word = "";
-    selectWord();
-    document.getElementById("blanks").innerHTML = blankWord;
-    document.getElementById("numGuesses").innerHTML = guessesRemaining;
-    document.getElementById("letGuesses").innerHTML = "~";
+function lose() {
+  lettersGuessed = []
+  guessesRemaining = 10
+  lettersGuessed = []
+  winningWord = ""
+  blankWord = ""
+  word = ""
+  selectWord()
+  document.getElementById("blanks").innerHTML = blankWord
+  document.getElementById("numGuesses").innerHTML = guessesRemaining
+  document.getElementById("letGuesses").innerHTML = "~"
 }
 
-function selectWord(){
-    winningWord = possibleWords[Math.floor(Math.random()*possibleWords.length)];
-    console.log(winningWord);
-    for(let i=0; i<winningWord.length; i++){
-        blankWord = blankWord + "_";
+function selectWord() {
+  winningWord = possibleWords[Math.floor(Math.random() * possibleWords.length)]
+  console.log(winningWord)
+  for (let i = 0; i < winningWord.length; i++) {
+    blankWord = blankWord + "_"
+  }
+  document.getElementById("blanks").innerHTML = blankWord
+  document.getElementById("numGuesses").innerHTML = guessesRemaining
+  document.getElementById("letGuesses").innerHTML = "~"
+}
+
+function guessLetter(guess) {
+  const formattedGuess = guess.toLowerCase()
+  if (isLetter(formattedGuess)) {
+    if (lettersGuessed.includes(formattedGuess)) return
+    for (let i = 0; i < winningWord.length; i++) {
+      if (formattedGuess === winningWord[i]) {
+        blankWord =
+          blankWord.substring(0, i) +
+          winningWord[i] +
+          blankWord.substring(i + 1)
+        document.getElementById("blanks").innerHTML = blankWord
+      }
+      if (i > 0 && blankWord == winningWord) {
+        win()
+        return
+      }
     }
-    document.getElementById("blanks").innerHTML = blankWord;
-    document.getElementById("numGuesses").innerHTML = guessesRemaining;
-    document.getElementById("letGuesses").innerHTML = "~";
-}
-
-function guessLetter(guess){
-    if(isLetter(guess.toLowerCase())){
-        for(let i=0; i<lettersGuessed.length; i++){
-            if(guess.toLowerCase() === lettersGuessed[i]) return;
-        }
-        for(let i=0; i<winningWord.length; i++){
-            if(guess.toLowerCase() === winningWord[i]){
-                blankWord = blankWord.substring(0,i) + winningWord[i] + blankWord.substring(i+1);
-                document.getElementById("blanks").innerHTML = blankWord;
-            }
-            if(i>0 && blankWord==winningWord){
-                win();
-                return;
-            }
-        }
-        guessesRemaining--;
-        document.getElementById("numGuesses").innerHTML = guessesRemaining;
-        lettersGuessed.push(guess);
-        document.getElementById("letGuesses").innerHTML = lettersGuessed;
-        if(guessesRemaining<1){
-            lose();
-        }
+    winningWord.guessesRemaining--
+    document.getElementById("numGuesses").innerHTML = guessesRemaining
+    lettersGuessed.push(guess)
+    document.getElementById("letGuesses").innerHTML = lettersGuessed
+    if (guessesRemaining < 1) {
+      lose()
     }
+  }
 }
 
-function isLetter(str){
-    return str.length === 1 && str.match(/[a-z]/i);
+function isLetter(str) {
+  return str.length === 1 && str.match(/[a-z]/i)
 }
 
-selectWord();
-
+selectWord()
